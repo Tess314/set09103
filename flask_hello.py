fc22173f95f4b590ec47453265afc13e538934da
Tess314 <40434810@live.napier.ac.uk>
2021-10-11 18:08:53 +0100

    added routing and 404 error

--- flask_hello.py
+++ flask_hello.py
@@ -1,6 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello, World!'
+@app.route("/")
+def root():
+	return "The default, 'root' route"
+
+@app.route("/hello/")
+def hello():
+    return "Hello Napier"
+
+@app.route("/goodbye/")
+def goodbye():
+    return "Goodbye World"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Error 404: Couldn't find the page you requested.", 404
+
+if __name__ == "main":
+    app.run(host='0.0.0.0', debug=True)
