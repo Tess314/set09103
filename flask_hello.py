96cf5c8e1442a9367355ec7b2bad2f1a7c751e2e
Tess314 <40434810@live.napier.ac.uk>
2021-11-03 20:21:38 +0000

    added code to read in static image from url

--- flask_hello.py
+++ flask_hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,13 @@ def root():
 @app.route("/hello/")
 def hello():
     return "Hello Napier"
+
+@app.route('/static-example/img')
+def static_example_img():
+    start = '<img src="'
+    url = url_for('static', filename='vmask.jpg')
+    end = '">'
+    return start+url+end, 200
 
 @app.route("/goodbye/")
 def goodbye():
