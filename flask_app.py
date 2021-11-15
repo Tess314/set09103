0ca926377178a2f9f5b3e8b39d93edcc6b35778d
Tess314 <40434810@live.napier.ac.uk>
2021-11-15 14:52:04 +0000

    added rudimentary login form

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,39 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def root():
 	return "TTTLE"
 
-@app.route("/login/")
-def hello():
-    return "LOG IN"
+@app.route("/login/", methods=['POST','GET'])
+def log_in():
+    if request.method == 'POST':
+        print(request.form)
+        username = request.form['username']
+        print(request.form)
+        password = request.form['password']
+        print(request.form)
+        branch = request.form['branch']
+        return "Hello, %s" % username
+    else:
+        page = '''
+        <html><body>
+        <form action="" method="post" name="form">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username"/><br><br>
+        <label for="password">Password:</label>
+        <input type="text" name="password" id="password"/><br><br>
+        <label for="branch">Branch:</label>
+        <select name="branch" id="branch">
+            <option value="piershill">Piershill</option>
+            <option value="central">Central Library</option>
+            <option value="portobello">Portobello</option>
+        </select><br><br>
+        <input type="submit" name="Log In" id="submit"/>
+        </form>
+        </body></html>'''
+
+        return page
 
 @app.route('/static-example')
 def static_example():
@@ -17,7 +43,7 @@ def static_example():
     return start+url+end, 200
 
 @app.route("/welcome/")
-def goodbye():
+def welcome():
     return "WELCOME"
 
 @app.errorhandler(404)
