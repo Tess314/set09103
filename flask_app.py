7690a4ec5dc0f3c7444beb166e62402f153069d8
Tess314 <40434810@live.napier.ac.uk>
2021-12-02 17:17:49 +0000

    rough db made, search functionality on way

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,28 @@
-from flask import Flask, url_for, request, render_template, redirect
+from flask import Flask, url_for, request, render_template, redirect, g
+import sqlite3
+
 app = Flask(__name__)
+db_location = 'var/books.db'
+
+def get_db():
+    db = getattr(g, 'db', None)
+    if db is None:
+        db = sqlite3.connect(db_location)
+        g.db = db
+    return db
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.route("/")
 def root():
@@ -20,10 +43,13 @@ def log_in():
 
 @app.route("/welcome/", methods=['POST','GET'])
 def welcome():
+    #db = get_db()
+    #db.commit()
+
     if request.method == 'POST':
         print(request.form)
         search = request.form['search']
-        return "You searched for: %s" % search
+        return "You searched for %s" % search
     else:
         return render_template('welcome.html')
 
---
+++ schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE if exists books;
+
+CREATE TABLE books (
+	title text,
+	author text,
+	synopsis text,
+	cover blob
+);
