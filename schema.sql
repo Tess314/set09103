f214969fd1d4f2b994aaeef762d2217b09c8cc8c
Tess314 <40434810@live.napier.ac.uk>
2021-12-05 19:06:57 +0000

    added auth db to schema file

--- schema.sql
+++ schema.sql
@@ -1,8 +1,15 @@
 DROP TABLE if exists books;
+DROP TABLE if exists auth;
 
 CREATE TABLE books (
 	title text,
 	author text,
 	synopsis text,
 	cover blob
+);
+
+CREATE TABLE auth (
+	username text,
+	password text,
+	branch text
 );
