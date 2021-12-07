270a35101ae4182cb6fcae8441d3d9e2a995fba0
Tess <71218139+Tess314@users.noreply.github.com>
2021-12-07 20:56:28 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,7 @@
 DROP TABLE if exists books;
-DROP TABLE if exists auth;
 
 CREATE TABLE books (
 	title text,
 	author text,
 	synopsis text
-);
-
-CREATE TABLE auth (
-	username text,
-	password text,
-	branch text
 );
